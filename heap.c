ff43171e2a62e42aae0926527bae12161975a9c7
Replit user <>
2023-09-23 19:33:59 +0000

    new try

--- heap.c
+++ heap.c
@@ -16,23 +16,42 @@ typedef struct Heap{
   int capac;
 } Heap;
 
-
-void* heap_top(Heap* pq){
-    return NULL;
-}
-
-
-
-void heap_push(Heap* pq, void* data, int priority){
-
-}
-
-
-void heap_pop(Heap* pq){
-
-}
+/*
+1. Implemente la función `Heap* createHeap()`. Esta función crea un nuevo dato de tipo Heap inicializando sus variables. Considere que la capacidad incial es de 3 casillas para el arreglo.
+> Recuerde reservar memoria para el Heap y también para el arreglo `heapArray`.
+*/
 
 Heap* createHeap(){
 
    return NULL;
 }
+
+/*
+2. Implemente la función `void* heap_top(Heap* pq)`. Esta función retorna el dato con mayor prioridad del montículo. 
+    > Recuerde que este dato se encuentra en la raíz el montículo, es decir, en la primera casilla del arreglo.
+*/
+
+void* heap_top(Heap* pq){
+    return NULL;
+}
+
+/*
+3. Implemente la función `void heap_push(Heap* pq, void* data, int p)`. Esta función inserta un nuevo dato con prioridad `p` en el montículo. Si no recuerda como insertar datos en el montículo puede ver las [diapositivas](https://docs.google.com/presentation/d/1ZjXWMf6g05WdICqvno_oyRvorjAThABgbRGbEqc7mYU/edit#slide=id.g55ac49ed61_0_167).
+
+    > Si el arreglo está lleno aumente la capacidad al **doble + 1** usando la función `realloc` así:
+    > `arreglo = realloc(arreglo, nueva_capacidad)`
+*/
+
+void heap_push(Heap* pq, void* data, int priority){
+  return;
+}
+
+/*
+4. Implemente la función `void heap_pop(Heap* pq)`. Esta función elimina el mayor elemento del montículo (la raíz). Si no recuerda el procedimiento puede ver las [diapositivas](https://docs.google.com/presentation/d/1ZjXWMf6g05WdICqvno_oyRvorjAThABgbRGbEqc7mYU/edit#slide=id.g10c6e3d52b_0_98).
+*/
+
+void heap_pop(Heap* pq){
+  return;
+}
+
+
